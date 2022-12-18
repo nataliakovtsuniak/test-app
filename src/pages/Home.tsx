bf245236e64db8f42afdc9975b6a2ac9f962e738
natalia <natalia.kovtsuniak@gmail.com>
2022-12-18 18:01:57 +0200

    add mui packages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import { useAppContext } from "../hooks/useAppContext";
+import Skeleton from "@mui/material/Skeleton";
 
 const Home: React.FC = (): JSX.Element => {
   const { count } = useAppContext();
@@ -10,6 +11,7 @@ const Home: React.FC = (): JSX.Element => {
     <section>
       <h1>Welcome!</h1>
       <p>Current count: {count}</p>
+      <Skeleton width={200} height={400} />
       <Link to="/count">Go to count page</Link>
     </section>
   );

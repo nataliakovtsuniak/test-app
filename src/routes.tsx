4603666e90bec9f8113351a83483e20cd9d888b8
natalia <natalia.kovtsuniak@gmail.com>
2022-12-20 10:20:12 +0200

    remove unused

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Count from "./pages/Count";
 import NotFound from "./pages/NotFound";
 
 export default (
   <Routes>
     <Route path="/" element={<Home />} />
-    <Route path="/count" element={<Count />} />
     <Route path="*" element={<NotFound />} />
   </Routes>
 );
